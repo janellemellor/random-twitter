df8adde09aa8552fb4ef0624ee5b5274182b6851
janelle mellor <janellemellor@gmail.com>
2020-03-24 13:49:58 -0700

    add failing post test

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -4,8 +4,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Tweet = require('../lib/models/Tweet');
 
-describe('app routes', () => {
+
+describe('Tweet routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -17,4 +19,37 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  //POST - new tweet
+  it('creates a new tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({
+        handle: 'my handle',
+        text: 'quotes by Ron Swanson'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: expect.any(String),
+          text: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
 });
+
+
+
+
+
+//POST /api/v1/tweets to create a new tweet
+
+//GET /api/v1/tweets to get all tweets
+
+//GET /api/v1/tweets/:id to get tweet by ID
+
+//PATCH /api/v1/tweets/:id to update a tweet's TEXT only
+
+//DELETE /api/v1/tweets/:id to delete a tweet
