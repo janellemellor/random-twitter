e2da96b1fff1c0feaa99d714ff6e0ea8cfc779bd
janelle mellor <janellemellor@gmail.com>
2020-03-28 10:53:00 -0700

    add failing post test for commments

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -4,8 +4,10 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Comment = require('../lib/models/Comment');
+const Tweet = require('../lib/models/Tweet');
 
-describe('app routes', () => {
+describe('comment routes', () => {
   beforeAll(() => {
     connect();
   });
@@ -17,4 +19,25 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('creates a new comment', () => {
+    return request(app)
+      .post('/api/v1/comments')
+      .send({
+        tweetId: new mongoose.Types.ObjectId(),
+        handle: 'my fake handle',
+        text: 'hello!'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          tweetId: expect.any(String),
+          handle: expect.any(String),
+          text: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+ 
 });
